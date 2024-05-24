0523492217902f0fcc0447d84505675c56536b2c
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-05-24 16:41:22 +0300

    Homework queries 19-21

---
+++ 01.Databases Introduction/19-21 query.sql
@@ -0,0 +1,23 @@
+SELECT * FROM [Towns]
+
+SELECT * FROM [Departments]
+
+SELECT * From [Employees]
+
+SELECT * FROM [Towns]
+ORDER BY [Name]
+
+SELECT * FROM [Departments]
+ORDER BY [Name]
+
+SELECT * FROM [Employees]
+ORDER BY [Salary] DESC
+
+SELECT [Name] FROM [Towns]
+ORDER BY [Name]
+
+SELECT [Name] FROM [Departments]
+ORDER BY [Name]
+
+SELECT [FirstName], [LastName], [JobTitle], [Salary] FROM [Employees]
+ORDER BY [Salary] DESC
